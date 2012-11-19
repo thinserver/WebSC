4f5895859ef736aeb6d2a29663e206037c7b1420
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-11-19 20:28:23 +0100

    key listing works

--- Makefile
+++ Makefile
@@ -6,7 +6,9 @@ favicon:
 	inkscape img/keycard.svg --export-png=img/keycard.png -w32 -h32
 	convert img/keycard.png img/favicon.ico
 
-svgs:
+key:
 	inkscape img/key.svg --export-png=img/key.png -w64 -h64
-	inkscape img/person.svg --export-png=img/person.png -w64 -h64
+	
+person:
+	inkscape img/person.svg --export-png=img/person.png -w45 -h45
 
--- keyman.py
+++ keyman.py
@@ -22,7 +22,6 @@ def keyring():
 	gpg = gpgme.Context()
 	keylist = gpg.keylist()
 	keys = ''
-	br = '<br/>\n'
 	for key in keylist:
 		subkey = key.subkeys[0]
 		title = ''
@@ -32,14 +31,11 @@ def keyring():
 			title +=	uid.name
 			if uid.email.strip() != '':
 				title += ' &lt;'+uid.email+'&gt;'
-#		for uid in key.uids:
+		for uid in key.uids:
+			content += uid_template % (uid.name, uid.email)
 #		for subkey in key.subkeys:
 		subkey = key.subkeys[0]
-		content +=	subkey_template % (pubkey_algo[subkey.pubkey_algo] + ', ' + \
-									str(subkey.length)+' bit' + br + \
-									subkey.keyid + br + \
-									subkey.fpr + br + \
-									str(subkey.expired))
+		content += subkey_template % (pubkey_algo[subkey.pubkey_algo] + ', ' + str(subkey.length)+' bit', subkey.keyid, subkey.fpr)
 		keys += key_template % (title, content)
 	return open('keyman/keyring.html').read() % (keys)
 
