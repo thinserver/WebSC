7c64a02ac3aa31e1648a4b87f67829ee06bd8032
Matthias <da-kaiser@der-ball-ist-rund.net>
2012-11-21 16:50:38 +0100

    show signatures

--- keyman.py
+++ keyman.py
@@ -16,27 +16,44 @@ pubkey_algo = {
 
 @bobo.query('/keyring')
 def keyring():
+	# load templates
 	key_template = open('keyman/key.html').read()
 	uid_template = open('keyman/uid.html').read()
 	subkey_template = open('keyman/subkey.html').read()
+	signature_template = open('keyman/signature.html').read()
+
+	# connect to GPG
 	gpg = gpgme.Context()
 	keylist = gpg.keylist()
 	keys = ''
+	
+	# list keys
 	for key in keylist:
 		subkey = key.subkeys[0]
 		title = ''
-		content = ''
+		certificate = ''
+		signatures = ''
+		
+		# get title
 		if len(key.uids) > 0:
 			uid = key.uids[0]
 			title +=	uid.name
 			if uid.email.strip() != '':
 				title += ' &lt;'+uid.email+'&gt;'
+		
+		# draw certificate
 		for uid in key.uids:
-			content += uid_template % (uid.name, uid.email)
+			certificate += uid_template % (uid.name, uid.email)
 #		for subkey in key.subkeys:
 		subkey = key.subkeys[0]
-		content += subkey_template % (pubkey_algo[subkey.pubkey_algo] + ', ' + str(subkey.length)+' bit', subkey.keyid, subkey.fpr)
-		keys += key_template % (title, content)
+		certificate += subkey_template % (pubkey_algo[subkey.pubkey_algo] + ', ' + str(subkey.length)+' bit', subkey.keyid, subkey.fpr)
+
+		# get all signatures for this certificate
+		# for signature in key.signatures:
+		signatures += signature_template % ('Matthias Bock', 'mail@matthiasbock.net', '12345')
+
+		# render
+		keys += key_template % (title, "C72A19AB", certificate, signatures)
 	return open('keyman/keyring.html').read() % (keys)
 
 @bobo.query('/keyman')
