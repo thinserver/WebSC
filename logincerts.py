f90dc4ca9d0fe68a8efa143b98a1d54292131cad
matthiasbock <mail@matthiasbock.net>
2012-12-27 16:31:16 +0100

    logincerts + SSH authorized_keys

--- logincerts.py
+++ logincerts.py
@@ -8,8 +8,9 @@ import bobo
 
 @bobo.query('/logincerts.py')
 def logincerts():
-	pkcs12 = "<h3>2 PKCS#12-Zertifikate:</h3><br/>\n"
+	pkcs12 = "<ul>\n"
 	for p12 in ["Florian Paintner", "Beate Paintner"]:
-		pkcs12 += p12+"<br/>\n"
-	ssh = '<h3>SSH-Zug&auml;nge:</h3><br/>'+open('/root/.ssh/authorized_keys').read()
-	return open('logincerts/certpile.html').read() % (pkcs12+'<hr/>'+ssh)
+		pkcs12 += "<li>"+p12+"</li>\n"
+	pkcs12 += "</ul>\n"
+	ssh = open('/root/.ssh/authorized_keys').read().strip()
+	return open('logincerts/logincerts.html').read() % (pkcs12, ssh)
